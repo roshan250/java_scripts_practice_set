50f43818f9237e75535d1ef00c14428783895f54
Roshan Sudhir Bhuskute <78207505+roshan250@users.noreply.github.com>
2026-01-29 07:17:35 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score =  true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let isLoggedIn = "0404"
+let valueInNumber = String(isLoggedIn);
+console.log(valueInNumber)
+
+/*
+"33" -> nan
+"33abc" -> NaN
+true -> 1; false -> 0
+"String" -> true
+"" -> false
+*/
