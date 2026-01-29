4b187e064080a1de0df660923933421f82aaa613
Roshan Sudhir Bhuskute <78207505+roshan250@users.noreply.github.com>
2026-01-29 06:06:14 +0000

    Learning about the Variables cost let var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =  144553
+let accountEmail = "roshanbhuskute@gmial.com"
+var accountPassword = "12345"
+accountCity = "jaipur" //in java script varible declare without data type
+let accountState;
+//accountId=222 //not allow to modification
+accountEmail= "sudhirbhuskute@gmail.com"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,  accountState]);
+
+/*
+
+*/
