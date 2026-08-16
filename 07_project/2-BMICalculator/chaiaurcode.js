e063d53be4b614d20f9cfb76b8e1db9c7c11e52e
roshanshivarix-25 <roshanshivarix@gmail.com>
2026-08-16 12:07:58 +0530

    Project JS

---
+++ 07_project/2-BMICalculator/chaiaurcode.js
@@ -0,0 +1,34 @@
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+
+    // Message 
+    let msg;
+    if(bmi<18.6){
+        msg = "Yor BMI Under Weight"
+    }
+    else if( bmi>18.6 && bmi<24.9 ){
+        msg = "Yor BMI Normla Weight"
+    }else if( bmi>24.9 ){
+        msg = "Yor BMI Over Weight"
+    }
+    message.innerHTML = `<span>${msg}</span>`;
+  }
+});
+
